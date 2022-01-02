323537a6a2d95ea2904c8a6e81e717e99d639c9a
Petr Zhizhin <piter.zh@gmail.com>
2022-01-02 13:39:51 +0300

    Fake services for Moscow voting

---
+++ fake_mdm/app/app.py
@@ -0,0 +1,98 @@
+import time
+import secrets
+
+from flask import Flask, request
+from functools import wraps
+
+from hashlib import blake2b, sha256
+
+app = Flask(__name__)
+app.config.from_object('config')
+
+def token_required(f):
+    @wraps(f)
+    def decorator(*args, **kwargs):
+        if 'x-application-token' not in request.headers:
+            return {"errorCode": 1, "errorMessage": "No token was provided"}
+        token = request.headers['x-application-token']
+        if token != app.config["MDM_GID_SERVICE_TOKEN"]:
+            return {"errorCode": 2, "errorMessage": "Wrong token was provided to MDM"}
+        return f(*args, **kwargs)
+    return decorator
+
+
+@app.route('/generate_gid', methods=["POST"])
+@token_required
+def generate_gid():
+    print('Got request: {}'.format(request.json))
+    print('Request headers: {}'.format(request.headers))
+    ssoId = request.json.get("ssoId")
+    if ssoId is None:
+        return {"errorCode": "3", "errorMessage": "Missing ssoId from request"}
+
+    secret = app.config["SECRET_KEY"]
+    hash_func = blake2b(key=bytes.fromhex(app.config["SECRET_KEY"]))
+    hash_func.update(ssoId.encode('utf-8'))
+
+    hash_value = hash_func.hexdigest()
+
+    return {"externalId": hash_value}
+
+
+def get_sha_signature():
+    timestamp = int(time.time() * 1000)
+    random_val = secrets.token_hex(32)
+    secret = app.config["SIGN_SECRET"]
+
+    for_sign_string = "{}|{}|{}".format(timestamp, random_val, secret).encode('utf-8')
+    h = sha256()
+    h.update(for_sign_string)
+    resulting_hash = h.hexdigest()
+    return {
+        'random': random_val,
+        'timestamp': timestamp,
+        'secureHash': resulting_hash,
+    }
+
+def get_response(return_code):
+    signature = get_sha_signature()
+    signature['externalId'] = secrets.token_hex(16)
+    signature['district'] = {'districtNumber': app.config['DISTRICT']}
+    signature['code'] = [return_code]
+    return signature
+
+
+@app.route('/checkBallot', methods=["POST"])
+@token_required
+def check_ballot():
+    print('Got request: {}'.format(request.json))
+    print('Request headers: {}'.format(request.headers))
+
+    ssoId = request.json.get("ssoId")
+    if ssoId is None:
+        return {"errorCode": "3", "errorMessage": "Missing ssoId from request"}
+
+    result = get_response(app.config['CHECK_BALLOT_SUCCESS_CODE'])
+
+    print('Returning {}'.format(result))
+    return result
+
+
+@app.route('/getBallot', methods=["POST"])
+@token_required
+def get_ballot():
+    print('Got request: {}'.format(request.json))
+    print('Request headers: {}'.format(request.headers))
+
+    ssoId = request.json.get("ssoId")
+    if ssoId is None:
+        return {"errorCode": "3", "errorMessage": "Missing ssoId from request"}
+
+    result = get_response(app.config['GET_BALLOT_SUCCESS_CODE'])
+
+    print('Returning {}'.format(result))
+    return result
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
---
+++ fake_sudir/app/website/app.py
@@ -0,0 +1,71 @@
+import os
+import time
+from flask import Flask
+from .models import db, User, OAuth2Client
+from .oauth2 import config_oauth
+from .routes import bp
+
+def create_db_elements():
+    if User.query.count() > 0:
+        return
+    admin_user = User(
+            username='admin', 
+            first_name='admin_first', 
+            last_name='admin_last', 
+            middle_name='admin_middle', 
+            mail='admin_mail', 
+            mobile='admin_mobile'
+    )
+    db.session.add(admin_user)
+    db.session.commit()
+
+    client = OAuth2Client(
+        client_id='deg_client_id',
+        client_secret='deg_client_secret',
+        client_id_issued_at=int(time.time()),
+        user_id=admin_user.id,
+    )
+    client.set_client_metadata({
+        "client_name": 'deg_client',
+        "client_uri": 'http://deg_client_uri',
+        "grant_types": ['authorization_code'],
+        "redirect_uris": ['http://localhost/fake_redirect_uri'],
+        "response_types": ['code'],
+        "scope": 'openid profile contacts',
+        "token_endpoint_auth_method": 'client_secret_post',
+    })
+    db.session.add(client)
+    db.session.commit()
+
+
+def create_app(config=None):
+    app = Flask(__name__)
+
+    # load default configuration
+    app.config.from_object('website.settings')
+
+    # load environment configuration
+    if 'WEBSITE_CONF' in os.environ:
+        app.config.from_envvar('WEBSITE_CONF')
+
+    # load app specified configuration
+    if config is not None:
+        if isinstance(config, dict):
+            app.config.update(config)
+        elif config.endswith('.py'):
+            app.config.from_pyfile(config)
+
+    setup_app(app)
+    return app
+
+
+def setup_app(app):
+    # Create tables if they do not exist already
+    @app.before_first_request
+    def create_tables():
+        db.create_all()
+        create_db_elements()
+
+    db.init_app(app)
+    config_oauth(app)
+    app.register_blueprint(bp, url_prefix='')
