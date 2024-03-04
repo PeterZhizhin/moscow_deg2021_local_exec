cc51ff4b1b75607c8c10453a2e2862c8f7167e48
Petr Zhizhin <piter.zh@gmail.com>
2024-03-05 00:00:42 +0100

    Added re-encryption decryption functions

--- fake_blockchain_connector/blockchain_votes_processor/re_encrypt_message.py
+++ fake_blockchain_connector/blockchain_votes_processor/re_encrypt_message.py
@@ -1,21 +1,27 @@
-from collections.abc import Callable
+import dataclasses
+import logging
 import os
 import sys
+
+import nacl.exceptions
 import nacl.public
 import nacl.utils
-import dataclasses
 import exonum_client
 from exonum_client import crypto as exonum_crypto
+from google.protobuf import message as protobuf_message
 
 # Add compiled protos to the current path, since it's required by protoc
 sys.path.append(os.path.join(os.path.dirname(__file__), "exonum_modules", "main"))
 
 from exonum_modules.main import transactions_pb2
 from exonum_modules.main import custom_types_pb2
+from exonum_modules.main import schema_pb2
 from exonum_modules.main.exonum import messages_pb2
 
 _INSTANCE_ID = 1001
 _TX_STORE_BALLOT_METHOD_ID = 6
+
+logger = logging.getLogger(__file__)
 
 
 @dataclasses.dataclass(frozen=True)
@@ -34,21 +40,19 @@ class ReEncryptionResult:
 def _parse_store_tx_from_raw_hex(
     store_ballot_transaction: bytes,
 ) -> transactions_pb2.TxStoreBallot:
-    signed_message = messages_pb2.SignedMessage()
-    signed_message.ParseFromString(store_ballot_transaction)
-    core_message = messages_pb2.CoreMessage()
-    core_message.ParseFromString(signed_message.payload)
-    tx_store_ballot = transactions_pb2.TxStoreBallot()
-    tx_store_ballot.ParseFromString(core_message.any_tx.arguments)
-
-    return tx_store_ballot
-
-
-def _validate_tx_store_ballot(tx_store_ballot: transactions_pb2.TxStoreBallot):
-    if not tx_store_ballot.district_id:
-        raise ValueError(f"Got invalid district ID in proto: {tx_store_ballot}")
-    if not tx_store_ballot.voting_id:
-        raise ValueError(f"Got invalid voting ID in proto: {tx_store_ballot}")
+    try:
+        signed_message = messages_pb2.SignedMessage()
+        signed_message.ParseFromString(store_ballot_transaction)
+        core_message = messages_pb2.CoreMessage()
+        core_message.ParseFromString(signed_message.payload)
+        tx_store_ballot = transactions_pb2.TxStoreBallot()
+        tx_store_ballot.ParseFromString(core_message.any_tx.arguments)
+        return tx_store_ballot
+    except protobuf_message.DecodeError as e:
+        logging.warning(
+            f"Got protobuf decode error when parsing store ballot tx, returning empty tx: {e}"
+        )
+        return transactions_pb2.TxStoreBallot()
 
 
 def _encrypt_on_public_key(data: bytes, public_key: nacl.public.PublicKey):
@@ -98,8 +102,9 @@ def _tx_store_ballot_to_exonum_message_hex(
         msg=tx_store_ballot,
     )
     exonum_message.sign(new_voter_key)
-    assert exonum_message.signed_raw() is not None
-    transaction_hex = exonum_message.signed_raw().hex()
+    signed_raw = exonum_message.signed_raw()
+    assert signed_raw is not None
+    transaction_hex = signed_raw.hex()
     voter_address_hex = new_voter_key.public_key.value.hex()
     return ReEncryptionResult(
         transaction_hex=transaction_hex,
@@ -122,7 +127,6 @@ def re_encrypt_blockchain_message_with_sid(
     if not district_id:
         raise ValueError("Invalid district_id")
     tx_store_ballot = _parse_store_tx_from_raw_hex(store_ballot_transaction)
-    _validate_tx_store_ballot(tx_store_ballot)
 
     encrypted_choice_serialized = tx_store_ballot.encrypted_choice.SerializeToString()
     encrypted_choice_encrypted = _encrypt_on_public_key(
@@ -140,3 +144,84 @@ def re_encrypt_blockchain_message_with_sid(
     return _tx_store_ballot_to_exonum_message_hex(
         tx_store_ballot=re_encrypted_tx_store_ballot
     )
+
+
+def _decrypt_message(
+    message: bytes,
+    nonce: bytes,
+    public_key_bytes: bytes,
+    private_key: nacl.public.PrivateKey,
+) -> bytes | None:
+    try:
+        public_key = nacl.public.PublicKey(public_key_bytes)
+        box = nacl.public.Box(private_key, public_key)
+        return box.decrypt(message, nonce=nonce)
+    except nacl.exceptions.CryptoError as e:
+        logging.warning(
+            f"Got crypto error when decrypting a message, returning None: {e}"
+        )
+    return None
+
+
+def _decode_choices_proto(choices_encoded: bytes) -> list[int] | None:
+    if len(choices_encoded) < 2:
+        logging.warning(f"Got too short choices message: {choices_encoded.hex()}")
+        return None
+    offset = ((choices_encoded[0] << 8) | choices_encoded[1]) + 2
+    if len(choices_encoded) < offset:
+        logging.warning(
+            f"Got choices message shorter than offset: {choices_encoded.hex()}, offset: {offset}"
+        )
+        return None
+    original_message = choices_encoded[offset:]
+    choices_proto = schema_pb2.Choices()
+    try:
+        choices_proto.ParseFromString(original_message)
+    except protobuf_message.DecodeError:
+        return None
+    return list(choices_proto.data)
+
+
+def _decrypt_tx_encrypted_choice(
+    tx: transactions_pb2.TxEncryptedChoice,
+    private_key: nacl.public.PrivateKey,
+) -> bytes | None:
+    return _decrypt_message(
+        message=tx.encrypted_message,
+        nonce=tx.nonce.data,
+        public_key_bytes=tx.public_key.data,
+        private_key=private_key,
+    )
+
+
+def decrypt_re_encrypted_tx(
+    *,
+    re_encrypted_tx_encrypted_vote: transactions_pb2.TxEncryptedChoice,
+    re_encryption_private_key: nacl.public.PrivateKey,
+    first_layer_private_key: nacl.public.PrivateKey,
+) -> list[int] | None:
+    first_layer_decrypted = _decrypt_tx_encrypted_choice(
+        re_encrypted_tx_encrypted_vote, re_encryption_private_key
+    )
+
+    if first_layer_decrypted is None:
+        logging.warning("Got second layer None decryption")
+        return None
+
+    first_layer_tx = transactions_pb2.TxEncryptedChoice()
+    try:
+        first_layer_tx.ParseFromString(first_layer_decrypted)
+    except protobuf_message.DecodeError as e:
+        logging.warning(f"Got protobuf decode error, returning None: {e}")
+        return None
+
+    second_layer_decrypted = _decrypt_tx_encrypted_choice(
+        first_layer_tx,
+        first_layer_private_key,
+    )
+
+    if second_layer_decrypted is None:
+        logging.warning("Got first layer None decryption")
+        return None
+
+    return _decode_choices_proto(second_layer_decrypted)
